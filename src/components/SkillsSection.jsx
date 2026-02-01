5c546cd7da0bec66982f4e3f6c3138292177eb53
Shumye Ayalneh <shmuye27@gmail.com>
2026-02-01 10:30:12 +0300

    nav background update

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -64,24 +64,12 @@ export const SkillsSection = () => {
           {filteredSkills.map((skill, key) => (
             <div
               key={key}
-              className="bg-card p-6 rounded-lg shadow-xs card-hover"
+              className="bg-card p-4 rounded-lg shadow-xs card-hover"
             >
-              <div className="text-left mb-4">
+              <div className="flex items-center justify-center">
                 <h3 className="font-semibold text-lg"> {skill.name}</h3>
               </div>
-              <div className="w-full bg-secondary/50 h-2 rounded-full overflow-hidden">
-                <div
-                  className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out]"
-                  style={{ width: skill.level + "%" }}
-                />
-              </div>
-
-              {/* <div className="text-right mt-1">
-                <span className="text-sm text-muted-foreground">
-                  {skill.level}%
-                </span>
-              </div> */}
-            </div>
+          </div>
           ))}
         </div>
       </div>
